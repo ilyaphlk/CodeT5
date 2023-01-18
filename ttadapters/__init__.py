d43bc7e9c24bfed8fe1c71eb756ea44b8198b059
Ilya Pakhalko <ipahalko@gmail.com>
2023-01-19 00:53:20 +0100

    try fixing imports

--- ttadapters/__init__.py
+++ ttadapters/__init__.py
@@ -1,5 +1,5 @@
 from .third_party.models.t5.modeling_t5 import T5ForConditionalGeneration
 from .third_party.models.t5.configuration_t5 import T5Config
 from .utils.utils import get_adapter_config, modify_model_after_init
-from training_args import AdapterTrainingArguments, TrainingArguments, DataTrainingArguments, ModelArguments
+from .training_args import AdapterTrainingArguments, TrainingArguments, DataTrainingArguments, ModelArguments
 
