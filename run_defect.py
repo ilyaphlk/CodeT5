e56516d8bf17706a77c218b4ffb2ec10cbd13292
Ilya Pakhalko <ipahalko@gmail.com>
2023-01-19 00:38:34 +0100

    fix reading tt config

--- run_defect.py
+++ run_defect.py
@@ -150,13 +150,10 @@ def main():
 
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments,
                                AdapterTrainingArguments))
-    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
-        # If we pass only one argument to the script and it's the path to a json file,
-        # let's parse it to get our arguments.
-        model_args, data_args, training_args, adapter_args = parser.parse_json_file(
-            json_file=os.path.abspath(sys.argv[1]))
-    else:
-        model_args, data_args, training_args, adapter_args = parser.parse_args_into_dataclasses()
+    for sys_arg in sys.argv:
+        if sys_arg.endswith(".json"):
+            model_args, data_args, training_args, adapter_args = parser.parse_json_file(
+                json_file=os.path.abspath(sys.argv[1]))
 
     training_args.output_dir = f"outputs/{data_args.task_name}/{training_args.experiment_name}"
 
